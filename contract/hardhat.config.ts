0376bac0bdb7c9c9fecdf3b81e5d4d7b45c2b9de
Wang Jun <wangjundot@gmail.com>
2022-10-08 19:24:05 +0800

    add main net

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -13,9 +13,13 @@ const { API_URL, PRIVATE_KEY } = process.env;
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
-    solanadevnet: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`]
+    devnet: {
+      url: process.env.DEV_API_URL,
+      accounts: [`0x${process.env.DEV_PRIVATE_KEY}`]
+    },
+    onlinenet: {
+      url: process.env.API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
     }
   }
 };
